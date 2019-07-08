db986dada85ae2a62091fecf12954817ecde2767
Carl Espe <carl@cpespe.com>
2019-07-08 09:51:51 -0700

    Write three failing tests for runWithDebugger

--- runWithDebugger/runWithDebugger-tests.js
+++ runWithDebugger/runWithDebugger-tests.js
@@ -1,4 +1,18 @@
 // See runWithDebugger.md for notes on the exercise
 
-tests({});
+tests({
+	'It should run debugger on supplied callback': function() {
+		function sayHi() {
+			return "Hi!";
+		}
+		var result = runWithDebugger(sayHi);
+		eq(result, "Hi!");
+	},
+	'It should run callback': function() {
+		fail();
+	},
+	'It should accept optional array parameter and pass values to callback': function() {
+		fail();
+	}
+});
 
