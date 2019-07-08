0745fe637633dd3eb90214c4b1e0eb2b716f168d
Carl Espe <carl@cpespe.com>
2019-07-08 09:55:53 -0700

    Rewrite tests to add debugger last

--- runWithDebugger/runWithDebugger-tests.js
+++ runWithDebugger/runWithDebugger-tests.js
@@ -1,17 +1,17 @@
 // See runWithDebugger.md for notes on the exercise
 
 tests({
-	'It should run debugger on supplied callback': function() {
+	'It should run supplied callback': function() {
 		function sayHi() {
 			return "Hi!";
 		}
 		var result = runWithDebugger(sayHi);
 		eq(result, "Hi!");
 	},
-	'It should run callback': function() {
+	'It should accept optional array parameter and pass values to callback': function() {
 		fail();
 	},
-	'It should accept optional array parameter and pass values to callback': function() {
+	'It should run debugger on callback': function() {
 		fail();
 	}
 });
