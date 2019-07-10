817b8a47deabcdaf54082892897302773a954a91
Carl Espe <carl@cpespe.com>
2019-07-10 12:54:05 -0700

    Write four failing tests for basic librarySystem

--- librarysystem/librarysystem-tests.js
+++ librarysystem/librarysystem-tests.js
@@ -1,3 +1,17 @@
 tests({
-	'': function() {}
+	'librarySystem should make itself available at window.librarySystem': function() {
+		fail();
+	},
+	'If given a library name and a callback, it should store callback for retrieval by name': function() {
+		fail();	
+	},
+	'The callback should return the named library': function() {
+		fail();
+	},
+	"If callback is given, librarySystem should return undefined": function() {
+		fail();
+	},
+	"If only a library name is given, librarySystem should return the named library": function() {
+		fail();
+	},
 });
