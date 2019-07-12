865960d56cc4044e4d6afe618b0ccbc274cc2db6
Carl Espe <carl@cpespe.com>
2019-07-12 12:41:23 -0700

    Write seven failing tests for librarySystem-v2

--- librarysystem-v2/librarysystem-v2-tests.js
+++ librarysystem-v2/librarysystem-v2-tests.js
@@ -1,16 +1,28 @@
- // It's awkward to insert the dependency array in parameter two. It should be optional at parameter three because not
- // all libraries have dependencies. That follows naturally from the TDD already done.
- 
+// To allow loading out of order, will need to save dependency callbacks instead of running them right away in
+// the create step.
+// Run the callbacks to load dependencies in the use step.
+// The other requirement is that each library callback is run only once. Why?
 
 tests({
-	'If given a library name and a getLibrary callback, it should store library for retrieval by name': function() {
+	"It should take a library name of type String as its first parameter.": function() {
+		fail();
+	},
+	"Its optional second parameter should be a 'getLibrary' callback that returns the library.": function() {
+		fail();
+	},
+	"If given just the library name and callback, it should store library for retrieval by name.": function() {
+		fail();
 		function getAppLibrary() {
 			return 'app library';
 		}
 		librarySystem('app', getAppLibrary);
 		eq(libraries['app'], 'app library' );
 	},
-	"If called with optional array of dependencies, it should load the dependencies into library": function() {
+	"Its optional third parameter should be an array of library names of type String to load as dependencies.": function() {
+		fail();
+	},
+	"If called with optional array of dependencies, it should store the array and callback for later use.": function() {
+		fail();
 		function getDependencyLibrary() {
 			return 'loaded dependency';
 		}
@@ -22,7 +34,8 @@ tests({
 		librarySystem('app', getAppLibrary, ['dependency']);		// library with one dependency
 		eq(libraries['app'], 'app library with loaded dependency');
 	},
-	"If only given library name, it should return the named library with dependencies loaded, if any": function() {
+	"If only given library name, it should return the named library with dependencies loaded, if any.": function() {
+		fail();
 		var result = librarySystem('dependency');
 		eq(result, 'loaded dependency');
 
@@ -51,5 +64,8 @@ tests({
 
 		result = librarySystem('workBlurb');
 		eq(result, 'Gordon works at Watch and Code');
+	},
+	"It should not run 'getLibrary' callback more than once for each library.": function() {
+		fail();
 	}
 });
