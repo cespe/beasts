6e7cf6feb174d100acd81d4974335a6f69e3957c
Carl Espe <carl@cpespe.com>
2019-07-15 11:11:11 -0700

    Code librarySystem v2 to pass all tests

--- librarysystem-v2/librarysystem-v2-tests.js
+++ librarysystem-v2/librarysystem-v2-tests.js
@@ -5,67 +5,129 @@
 
 tests({
 	"It should take a library name of type String as its first parameter.": function() {
-		fail();
-	},
-	"Its optional second parameter should be a 'getLibrary' callback that returns the library.": function() {
-		fail();
-	},
-	"If given just the library name and callback, it should store library for retrieval by name.": function() {
-		fail();
-		function getAppLibrary() {
-			return 'app library';
+		try {
+			librarySystem({name: "myLibrary"});
+		} catch (error) {
+			eq(error.name, "TypeError");
+			eq(error.message, "libraryName must be a string");
 		}
-		librarySystem('app', getAppLibrary);
-		eq(libraries['app'], 'app library' );
 	},
-	"Its optional third parameter should be an array of library names of type String to load as dependencies.": function() {
-		fail();
+	//"Its optional second parameter should be a 'getLibrary' callback that returns the library.": function() {
+		// Can't see how to test this. Could require it to be a function but the function could return garbage.
+		//fail();
+	//},
+	"If given just the library name and 'getLibrary' callback, it should store library for retrieval by name.": function() {
+		function getApp1Library() {
+			return 'app library one';
+		}
+		librarySystem('app1', getApp1Library);
+		eq(libraries['app1'], 'app library one' );
+		eq('app1' in libraryDependencies, false);
+		eq('app1' in libraryCallbacks, false);
 	},
-	"If called with optional array of dependencies, it should store the array and callback for later use.": function() {
-		fail();
-		function getDependencyLibrary() {
-			return 'loaded dependency';
+	"Its optional third parameter 'dependencyNames' should be an array.": function() {
+		try {
+			librarySystem('testApp', function() {}, {});
+		} catch (error) {
+			eq(error.name, "TypeError");
+			eq(error.message, "dependencyNames must be an array");
 		}
-		librarySystem('dependency', getDependencyLibrary);			// simple library with no dependencies
-		
-		function getAppLibrary(dependency) {
-			return 'app library'+ ' with ' + dependency;
+	},
+	"Each element of 'dependencyNames' array should be of type String.": function() {
+		try {
+			librarySystem('testApp', function() {}, ['depOne', 'depTwo', {lib: 'depThree'}]);
+		} catch (error) {
+			eq(error.name, "TypeError");
+			eq(error.message, "each element of dependencyNames array must be a string");
 		}
-		librarySystem('app', getAppLibrary, ['dependency']);		// library with one dependency
-		eq(libraries['app'], 'app library with loaded dependency');
+	},
+	"If called with 'dependencyNames' array, it should store the array and 'getLibrary' callback for later use.": function() {
+		function getApp2Library(dependency2) {
+			return 'app library two'+ ' with ' + dependency2;
+		}
+		var app2Dependencies = ['dependency2'];
+		librarySystem('app2', getApp2Library, app2Dependencies);	// library with one dependency
+		eq(libraryDependencies['app2'], app2Dependencies);			// stored to load later
+		eq(libraryCallbacks['app2'], getApp2Library);
+		eq('app2' in libraries, false);
+
+		function getDependency2Library() {
+			return 'loaded dependency two';
+		}
+		librarySystem('dependency2', getDependency2Library);		// simple library with no dependencies
+		eq(libraries['dependency2'], 'loaded dependency two');		// loads immediately
+		eq('dependency2' in libraryDependencies, false);
+		eq('dependency2' in libraryCallbacks, false);
 	},
 	"If only given library name, it should return the named library with dependencies loaded, if any.": function() {
-		fail();
-		var result = librarySystem('dependency');
-		eq(result, 'loaded dependency');
+		var result = librarySystem('app2');							// library with one dependency
+		eq(result, 'app library two with loaded dependency two');
 
-		var result = librarySystem('app');
-		eq(result, 'app library with loaded dependency');
+		var result = librarySystem('dependency2');
+		eq(result, 'loaded dependency two');
 
-		// set up
-		librarySystem('name', function() {
+		// test case from the exercise
+		librarySystem('workBlurb', function(fullname, company) {		// library with two dependencies of which one
+			return fullname + ' works at ' + company;					// also has two dependencies
+		}, ['fullname', 'company']);
+
+		librarySystem('fullname', function(firstname, lastname) {
+			return firstname + " " + lastname;
+		}, ['firstname', 'lastname']);
+		
+		librarySystem('firstname', function() {
 			return 'Gordon';
+		});
+
+		librarySystem('lastname', function() {
+			return 'Zhu';
 		});
 
 		librarySystem('company', function() {
 			return 'Watch and Code';
 		});
 
-		librarySystem('workBlurb', function(name, company) {		// library with two dependencies
-			return name + ' works at ' + company;
-		}, ['name', 'company']);
-		
-		// tests
-		result = librarySystem('name');
+		result = librarySystem('workBlurb');
+		eq(result, 'Gordon Zhu works at Watch and Code');
+
+		result = librarySystem('fullname');
+		eq( result, 'Gordon Zhu');
+
+		result = librarySystem('lastname');
+		eq(result, 'Zhu');
+
+		result = librarySystem('firstname');
 		eq(result, 'Gordon');
 
 		result = librarySystem('company');
 		eq(result, 'Watch and Code');
+	},
+	"It should throw a ReferenceError if a library that is called for loading is undefined.": function() {
+		function getApp3Library(dependency3, missingDependency) {
+			return 'app library three'+ ' with ' + dependency3 + " " + missingDependency;
+		}
+		var app3Dependencies = ['dependency3', 'missingDependency'];
+		librarySystem('app3', getApp3Library, app3Dependencies);	// library with one missing dependency
 
-		result = librarySystem('workBlurb');
-		eq(result, 'Gordon works at Watch and Code');
+		function getDependency3Library() {
+			return 'loaded dependency three';
+		}
+		librarySystem('dependency3', getDependency3Library);
+
+		try {
+			result = librarySystem('app3');
+		} catch (error) {
+			eq(error.name, "ReferenceError");
+			eq(error.message, "missingDependency library is missing");
+		}
 	},
 	"It should not run 'getLibrary' callback more than once for each library.": function() {
-		fail();
+		function newWorkBlurbCallback(fullname, company) {
+			return "workBlurb 2.0" + " for " + fullname + " at " + company;
+		}
+		librarySystem('workBlurb', newWorkBlurbCallback, ['fullname', 'company']);
+		result = librarySystem('workBlurb');
+		eq(result, "Gordon Zhu works at Watch and Code");	// original library, not the replacement from changed callback
 	}
 });
+
--- librarysystem-v2/librarysystem-v2.js
+++ librarysystem-v2/librarysystem-v2.js
@@ -1,22 +1,54 @@
-// Beasts 4. librarySystem with dependencies
+// Beasts 5. Creating libraries out of order
 
 // Order of parameters for librarySystem is different from Gordon's version. It makes more
 // sense to me to put optional dependency array last, since not all libraries have dependencies.
 
 var libraries = {};
+var libraryDependencies = {};
+var libraryCallbacks = {};
 
-function librarySystem(libraryName, getLibraryCallback, /* optional */ dependencyNamesArray) {
+function librarySystem(libraryName, /* optional */ getLibrary, /* optional */ dependencyNames) {
+	if (typeof libraryName !== "string") {
+		throw new TypeError("libraryName must be a string"); 
+	}
+	if (libraries[libraryName]) {
+		// don't store a library more than once
+		return libraries[libraryName];
+	}
 	if (arguments.length > 1) {
 		if (arguments.length === 2) {
-			libraries[libraryName] = getLibraryCallback();
+			// store library that has no dependencies
+			libraries[libraryName] = getLibrary();
 		} else {
-			var dependencies = dependencyNamesArray.map(function(dependencyName) {
-				return librarySystem(dependencyName);
-			});
-			libraries[libraryName] = getLibraryCallback.apply(this, dependencies);
+			if (!Array.isArray(dependencyNames)) {
+			throw new TypeError("dependencyNames must be an array");
+			}
+			for (var i = 0; i < dependencyNames.length; i++) {
+				if (typeof dependencyNames[i] !== "string") {
+					throw new TypeError("each element of dependencyNames array must be a string");
+				}
+			}
+			// wait to store library that has dependencies
+			libraryDependencies[libraryName] = dependencyNames;
+			libraryCallbacks[libraryName] = getLibrary;
 		}
 	} else {
-		return libraries[libraryName];
+		// called with libraryName only
+		if (libraryName in libraryDependencies) {
+			// retrieve callback and dependencies from storage
+			var dependencyNames = libraryDependencies[libraryName];
+			var getLibrary = libraryCallbacks[libraryName];
+			var dependencies = dependencyNames.map(function(dependencyName) {
+				return librarySystem(dependencyName);	// recursive for nested dependencies
+			});
+			// store library that has dependencies
+			libraries[libraryName] = getLibrary.apply(this, dependencies);
+			}
+		if (libraries[libraryName] === undefined) {
+			throw new ReferenceError(libraryName + " library is missing");
+		} else {
+			return libraries[libraryName];
+		}
 	}
 };
 
