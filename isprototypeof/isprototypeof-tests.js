7057b91aac622a041f7ac0ab3a7450365f33c1cf
Carl Espe <carl@cpespe.com>
2019-07-16 14:50:26 -0700

    Write five failing tests for isPrototypeOf

--- isprototypeof/isprototypeof-tests.js
+++ isprototypeof/isprototypeof-tests.js
@@ -1,4 +1,20 @@
 // Beasts 6. isPrototypeOf
 // See file isprototypeof-notes.js for spec notes and console log tests.
 
-
+tests({
+	"It should take a possible prototype object 'prototypeObject' as the first parameter.": function() {
+		fail();		
+	}
+	"It should throw a TypeError if prototypeObject is undefined or null.": function() {
+		fail();
+	},
+	"It should throw a SyntaxError if prototypeObject is a number.": function() {
+		fail();
+	},
+	"It should return true if prototypeObject is a prototype of 'targetObject', otherwise false.": function() {
+		fail();
+	},
+	"It should work even if the prototype chain for targetObject is more than one link deep.": function() {
+		fail();
+	}
+});
