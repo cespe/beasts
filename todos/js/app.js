a6055ff497d9b08c7a1171ca6f94d0b6af3eae20
Carl Espe <carl@cpespe.com>
2019-07-27 13:45:37 -0700

    Code array insertion and deletion

--- todos/js/app.js
+++ todos/js/app.js
@@ -4,7 +4,7 @@ var todos = [];
 
 function addNewTodo(todo) {
 	todos.push(todo);
-};
+}
 
 function Todo(entry) {
 	this.id = 'id1';
@@ -33,4 +33,17 @@ Todo.prototype.markCompleted = function(bool) {
 
 Todo.prototype.markDeleted = function(bool) {
 	this.deleted = bool;
+}
+Todo.prototype.addChild = function(child) {
+	this.children.push(child);
+}
+
+function insertTodo(array, todoInPlace, todoToInsert) {
+	var position = array.indexOf(todoInPlace) + 1;
+	array.splice(position, 0, todoToInsert);
+}
+
+function deleteTodo(array, todo) {
+	var position = array.indexOf(todo);
+	array.splice(position, 1);
 }
--- todos/js/todos-tests.js
+++ todos/js/todos-tests.js
@@ -112,6 +112,76 @@ tests({
 		eq(newTodo.deleted, false);
 		eq(todos[0].deleted, false);
 	},
+	"The app should have a way to add a child todo to a parent todo.": function() {
+		todos = []
+		newTodo = new Todo('Item 1');
+		addNewTodo(newTodo);
+		eq(newTodo.children.length, 0);
+		childTodo = new Todo('Item 1 child 1');
+		newTodo.addChild(childTodo);
+		eq(newTodo.children.length, 1);
+		eq(newTodo.children[0], childTodo);
+	},
+	"The app should have a way to insert a new todo after any todo in the array it is in.": function() {
+		todos = []
+		var todo1 = new Todo('Item 1');
+		addNewTodo(todo1);
+		eq(todos[0], todo1);
+		var todo2 = new Todo('Item 2');
+		addNewTodo(todo2);
+		eq(todos[1], todo2);
+		var todo3 = new Todo('Item 3');
+		addNewTodo(todo3);
+		eq(todos[2], todo3);
+		var todo4 = new Todo('Item 4 inserted after Item 1');
+		insertTodo(todos, todo1, todo4);
+		eq(todos[0], todo1);
+		eq(todos[1], todo4);
+		eq(todos[2], todo2);
+		eq(todos[3], todo3);
+
+		var child1 = new Todo('Item 1 child 1');
+		todo1.addChild(child1);
+		eq(todo1.children[0], child1);
+		var child2 = new Todo('Item 1 child 2');
+		todo1.addChild(child2);
+		eq(todo1.children[1], child2);
+		var child3 = new Todo('Item 1 child 3 inserted after child 1');
+		insertTodo(todo1.children, child1, child3);
+		eq(todo1.children[0], child1);
+		eq(todo1.children[1], child3);
+		eq(todo1.children[2], child2);
+
+	},
+	"The app should have a way to delete a todo from the array it is in.": function() {
+		todos = []
+		var todo1 = new Todo('Item 1');
+		addNewTodo(todo1);
+		eq(todos[0], todo1);
+		var todo2 = new Todo('Item 2');
+		addNewTodo(todo2);
+		eq(todos[1], todo2);
+		var todo3 = new Todo('Item 3');
+		addNewTodo(todo3);
+		eq(todos[2], todo3);
+		deleteTodo(todos, todo2);
+		eq(todos[0], todo1);
+		eq(todos[1], todo3);
+
+		var child1 = new Todo('Item 1 child 1');
+		todo1.addChild(child1);
+		eq(todo1.children[0], child1);
+		var child2 = new Todo('Item 1 child 2');
+		todo1.addChild(child2);
+		eq(todo1.children[1], child2);
+		var child3 = new Todo('Item 1 child 3');
+		todo1.addChild(child3);
+		eq(todo1.children[2], child3);
+		deleteTodo(todo1.children, child2);
+		eq(todo1.children[0], child1);
+		eq(todo1.children[1], child3);
+
+	},
 	"The app should display todos.": function() {
 		fail();
 	}
