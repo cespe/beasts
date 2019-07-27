530ed4f962a099bcbd59829655fc2dabdc7d6fb0
Carl Espe <carl@cpespe.com>
2019-07-27 09:48:35 -0700

    Code data structures and todo constructor

--- todos/js/app.js
+++ todos/js/app.js
@@ -1,3 +1,23 @@
 // Beasts 8. Nested todos
 
+var todos = [];
 
+function addNewTodo(todo) {
+	todos.push(todo);
+};
+
+function Todo(entry) {
+	this.id = 'id1';
+	if (typeof entry !== "string") {
+		this.entry = "";
+	} else {
+		this.entry = entry;
+	}
+	this.children = [];
+	this.deleted = false;
+	this.completed = false;
+}
+
+Todo.prototype.update = function(changedEntry) {
+	this.entry = changedEntry;
+}
--- todos/js/todos-tests.js
+++ todos/js/todos-tests.js
@@ -1,25 +1,85 @@
 // Beasts 8. Nested todos
+// Will need to protect properties of todo object in a closure
+// todo should be created with a Constructor
 
 tests({
-	"It should have storage for todos.": function() {
+	"The app should have a 'todos' array for storing todos.": function() {
+		var hasStorage = Array.isArray(todos);
+		eq(hasStorage, true);
+	},
+	"Todo should return a new todo object.": function() {
+		var newTodo = new Todo();
+		eq(typeof newTodo, "object");
+	},
+	"The app should have a way to add a todo object to the todos array.": function() {
+		todos = [];
+		newTodo = new Todo();
+		addNewTodo(newTodo);
+		eq(todos[0], newTodo);
+	},
+	"A todo object should be created with an 'id' property of type string to store an identifier.": function() {
+		todos = [];
+		newTodo = new Todo();
+		addNewTodo(newTodo);
+		eq(todos[0].id, 'id1');
+	},
+	"A todo id should be a unique value of type string.": function() {
 		fail();
 	},
-	"It should have a way to add a todo.": function() {
+	"Todo should take an entry of type string and store it in the todo object 'entry' property.": function() {
+		todos = [];
+		newTodo = new Todo('Item 1');
+		addNewTodo(newTodo);
+		eq(todos[0].entry, 'Item 1');
+	},
+	"If 'entry' argument is not a string or is missing, Todo should set todo entry property to an empty string.": function() {
+		todos = [];
+		newTodo = new Todo();
+		addNewTodo(newTodo);
+		eq(todos[0].entry, '');
+		secondTodo = new Todo([]);
+		addNewTodo(secondTodo);
+		eq(todos[1].entry, '');
+	},
+	"A todo object should be created with a 'children' property of type array to store nested todo objects.": function() {
+		todos = [];
+		newTodo = new Todo();
+		addNewTodo(newTodo);
+		childrenArray = todos[0].children;
+		eq(Array.isArray(childrenArray), true);
+		eq(childrenArray.length, 0);
+	},
+	"A todo object should be created with a 'deleted' property of type boolean set to false.": function() {
+		todos = [];
+		newTodo = new Todo();
+		addNewTodo(newTodo);
+		eq(todos[0].deleted, false);
+	},
+	"A todo object should be created with a 'completed' property of type boolean set to false.": function() {
+		todos = [];
+		newTodo = new Todo();
+		addNewTodo(newTodo);
+		eq(todos[0].completed, false);
+	},
+	"The app should have a way to update a todo entry.": function() {
+		todos = [];
+		newTodo = new Todo('Item 1');
+		addNewTodo(newTodo);
+		eq(todos[0].entry, 'Item 1');
+		newTodo.update('Item 1 updated');
+		eq(newTodo.entry, 'Item 1 updated');
+		eq(todos[0].entry, 'Item 1 updated');
+	},
+	"The app should have a way to mark a todo completed.": function() {
 		fail();
 	},
-	"It should have a way to add a child todo.": function() {
+	"The app should have a way to mark a todo selected.": function() {
 		fail();
 	},
-	"It should display todos.": function() {
+	"The app should have a way to mark a todo deleted.": function() {
 		fail();
 	},
-	"It should have a way to edit a todo.": function() {
-		fail();
-	},
-	"It should have a way to mark a todo complete.": function() {
-		fail();
-	},
-	"It should have a way to delete a todo.": function() {
+	"The app should display todos.": function() {
 		fail();
 	}
 });
